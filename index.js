a1e3ef40507fd73b1f74286105a237d004dec4d7
Abdul Khan <akhan17@outlook.com>
2020-09-22 20:31:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution here
+function findMatching(drivers, string) {
+  const newDrivers = drivers.filter(name => {
+    return name.toLowerCase() === string.toLowerCase();
+  })
+  return newDrivers
+}
